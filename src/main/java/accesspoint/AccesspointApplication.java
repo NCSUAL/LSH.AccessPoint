f3e6931f31443235b857140b3dfd369101487ba8
NCSUAL <ncsual@naver.com>
2025-07-09 18:29:08 +0900

    [init] 환경 설정

--- src/main/java/accesspoint/AccesspointApplication.java
+++ src/main/java/accesspoint/AccesspointApplication.java
@@ -1,4 +1,4 @@
-package accesspoint.accesspoint;
+package accesspoint;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/accesspoint/AccesspointApplicationTests.java
+++ src/test/java/accesspoint/AccesspointApplicationTests.java
@@ -1,4 +1,4 @@
-package accesspoint.accesspoint;
+package accesspoint;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
